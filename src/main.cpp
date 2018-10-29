48eaa7b28de4f6182c3f2a5300b44994a980c5d2
Karthik N <karthik.chopper@gmail.com>
2018-10-28 20:11:16 -0700

    Adjusting the location of the data file for Netbeans

--- src/main.cpp
+++ src/main.cpp
@@ -175,7 +175,12 @@ int main() {
     vector<double> map_waypoints_dy;
 
     // Waypoint map to read from
-    string map_file_ = "../data/highway_map.csv";
+    //string map_file_ = "../data/highway_map.csv";
+    
+    //TODO: The location where the executable blob is generated is different when using Netbeans in Ubuntu,
+    //Updating the location of the map_file_ accordingly for it to pick up.
+    string map_file_ = "./data/highway_map.csv";
+    
     // The max s value before wrapping around the track back to 0
     double max_s = 6945.554;
 
